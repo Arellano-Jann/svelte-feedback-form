d92da7db2a1cae41d54cf6557fad855b77ae7556
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-03-20 13:24:49 -0700

    added components and imported

--- svelte-feedback-form/src/App.svelte
+++ svelte-feedback-form/src/App.svelte
@@ -1,8 +1,27 @@
 <script>
-	
+	import { FeedbackList, FeedbackItem } from './components/*'; // might be wrong?
+
+	let feedback = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Test1'
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Test12'
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Test123'
+	}
+	];
 </script>
 
 <main>
+	<FeedbackList {feedback} />
 	<h1>Feedback</h1>
 	<p>Thank you for your feedback!</p>
 </main>
